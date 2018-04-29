af0d39db7db90e317226ee1044cb8264d718a86f
Guillaume Bonheure <Guillaume Bonheure@192.168.0.123>
2018-04-29 11:53:35 +0200

    Added function descriptions

--- Teamleader Problem 1/discounts.php
+++ Teamleader Problem 1/discounts.php
@@ -1,7 +1,26 @@
 <?php
-
+/**
+ * This file contains all the functions used to check the given order for
+ * potential discounts the given customer may get. Every function receives the
+ * same input, being a customer, an order and the set of products. All these
+ * inputs are arrays.
+ * 
+ * The functions that actually return the discounts should be called the same
+ * as the elements of $available_discounts in the main file.
+ * 
+ * As of now, the output simply consists of print statements, but this can be
+ * changed to actually return a value.
+ * 
+ * @param   array   $customer
+ * @param   array   $order
+ * @param   array   $products
+ */
 function spent_over_1000($customer, $order, $products)
 {
+    /**
+     * Checks if the given customer has already spent over €1000.
+     * If so, a discount of 10% is awarded on the entire order. 
+     */
     if($customer['revenue'] > 1000)
     {
         echo '<pre>10% off the total order price</pre>';
@@ -10,6 +29,10 @@ function spent_over_1000($customer, $order, $products)
 
 function five_of_cat_2($customer, $order, $products)
 {   
+    /**
+     * Checks if the given order contains 5 of the same items from category 2.
+     * If so, a sixth such item is added to the order for free.
+     */
     $result = "";
     foreach($order['items'] as $index => $item)
     {
@@ -22,6 +45,10 @@ function five_of_cat_2($customer, $order, $products)
 
 function two_of_cat_1($customer, $order, $products)
 {
+    /**
+     * Checks if the given order contains at least 2 items from category 1.
+     * If so, a 20% discount is awarded on the cheapest item.
+     */
     $prices = array();
     
     foreach($order['items'] as $index => $item)
--- Teamleader Problem 1/index.php
+++ Teamleader Problem 1/index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,13 +5,37 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        
+        /**
+         * This file can be used by calling getDiscounts() with an order file,
+         * a customers file and a products file. This will print a summary of
+         * the order, followed by any discounts this order may merit.
+         * 
+         * This file requires discounts.php to be present in the working directory
+         * in order to properly function, otherwise no output will be generated.
+         * 
+         * One can add additional discounts by coding them in discounts.php
+         * and adding the name of the function that returns the discount to
+         * the variable $available_discounts.
+         * 
+         * @author Guillaume Bonheure
+         */
         $available_discounts = ['spent_over_1000','five_of_cat_2','two_of_cat_1'];
                                 
         require 'discounts.php';
         
         function getDiscounts($order_file, $customer_file, $product_file) 
         {
+            /**
+             * Iterates through every discount in $available_discounts.
+             * 
+             * @param   JSON File   $order_file     Specifies the file containing the order to be processed
+             * @param   JSON File   $customer_file  Specifies the file containing the customers
+             * @param   JSON File   $product_file   Specifies the file containing the available products
+             * 
+             * @return  Whatever the discount functions from discount.php generate.
+             *          Currently everything is simply printed, but this can be
+             *          modified to return an array of discounts, for example.
+             */
             global $available_discounts;
             $order = getData($order_file);
             $customers = getData($customer_file);
@@ -46,14 +64,19 @@ and open the template in the editor.
         
         function getData($data_file)
         {
+            /**
+             * Extracts the contents of a given file and uses the JSON decoder
+             * to put the data in an array.
+             * 
+             * @param   JSON File   $data_file  Specifies the file to extract the data from
+             * @return  The array containing data from $data_file
+             */
             $file = file_get_contents($data_file);
             
             $data = json_decode($file, true);
             
             return $data;
         }
-        
-        getDiscounts("order4.json", "customers.json", "products.json");
         ?>
     </body>
 </html>
